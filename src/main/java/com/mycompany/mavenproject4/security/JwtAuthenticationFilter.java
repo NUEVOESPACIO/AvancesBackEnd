b30026e7a17b287b4ded1869aff0edb852118f4c
NUEVOESPACIO <sebacap3@hotmail.com>
2025-10-05 16:57:18 -0300

    Agregado el SecurityConfig.java OK Build Ok RUN

--- src/main/java/com/mycompany/mavenproject4/security/JwtAuthenticationFilter.java
+++ src/main/java/com/mycompany/mavenproject4/security/JwtAuthenticationFilter.java
@@ -9,8 +9,10 @@ import java.util.Collections;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
