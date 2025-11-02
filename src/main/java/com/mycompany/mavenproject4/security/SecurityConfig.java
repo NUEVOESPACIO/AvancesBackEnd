d31fac98756fd7d3a4af685f370b4a08ff33113b
NUEVOESPACIO <sebacap3@hotmail.com>
2025-11-02 20:16:13 -0300

    He bajado un nivel de avanze porque no logro que se cargue al congigh los roles. El api HEllo lo dejo solo en authenticated para seguir probando

--- src/main/java/com/mycompany/mavenproject4/security/SecurityConfig.java
+++ src/main/java/com/mycompany/mavenproject4/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()      
+                        .requestMatchers("/api/**").authenticated()    
                         .anyRequest().authenticated()
                 )
                 
